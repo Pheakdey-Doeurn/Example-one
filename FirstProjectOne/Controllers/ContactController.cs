ca3ea6424ecd436b0313006aefb0c65e6a381398
Pheakdey Doeurn <pheakdey.96kh@gmail.com>
2023-11-25 16:13:29 +0700

    add project

---
+++ FirstProjectOne/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProjectOne.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ContentResult Greet()
+        {
+            return Content("Hello Everyone");
+        }
+        public int add()
+        {
+            int a = 5;
+            int b = 6;
+            int s = a + b;
+            return s;
+        }
+    }
+}
---
+++ FirstProjectOne/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProjectOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show you Home Page";
+            return View();
+        }
+        public ActionResult Display()
+        {
+            return View();
+        }
+        
+    }
+}
